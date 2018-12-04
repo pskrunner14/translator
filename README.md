c601fb1258337ee60fd3b93b33edd90c19146d13
Prabhsimran Singh <pskrunner14@gmail.com>
2018-12-04 19:51:31 +0530

    minor fixes (#7)

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/9020f72ab88b4f05b2082b94e7efe6de)](https://app.codacy.com/app/pskrunner14/translator?utm_source=github.com&utm_medium=referral&utm_content=pskrunner14/translator&utm_campaign=Badge_Grade_Dashboard)
 
-*Translator* is a Machine Translation Autoencoder built using Seq2Seq Recurrent Nets with Attention in PyTorch.
+Machine translation is a sub-field of computational linguistics that investigates the use of software to translate text or speech from one language to another. On a basic level, it performs simple substitution of words in one language for words in another, but that alone usually cannot produce a good translation of a text because recognition of whole phrases and their closest counterparts in the target language is needed. Solving this problem with corpus statistical, and neural techniques is a rapidly growing field that is leading to better translations, handling differences in linguistic typology, translation of idioms, and the isolation of anomalies.
+
+A deep learning based approach to MT, Neural Machine Translation has made rapid progress in recent years. **Translator** is a Machine Translation Autoencoder built using Seq2Seq Recurrent Nets (LSTM, GRU) with Attention in PyTorch.
 
 ## Dataset
 
@@ -148,13 +150,23 @@ Target: i m your boyfriend aren t i ?
 Predicted: i m the one aren t i ? <EOS>
 ```
 
+## TO-DO
+
+- [x] Decoder Attention
+- [ ] Beam Search Decoding
+- [ ] GloVe Word Embeddings
+
 ## References
 
-* [Effective Approaches to Attention-based Neural Machine Translation](https://arxiv.org/abs/1508.04025)
-* [Learning Phrase Representations using RNN Encoder-Decoder for Statistical Machine Translation](https://arxiv.org/abs/1406.1078)
-* [Sequence to Sequence Learning with Neural Networks](https://arxiv.org/abs/1409.3215)
+- [Effective Approaches to Attention-based Neural Machine Translation](https://arxiv.org/abs/1508.04025)
+- [Learning Phrase Representations using RNN Encoder-Decoder for Statistical Machine Translation](https://arxiv.org/abs/1406.1078)
+- [Sequence to Sequence Learning with Neural Networks](https://arxiv.org/abs/1409.3215)
 
 ## Built With
 
-* Python
-* PyTorch
+- Python
+- PyTorch
+
+## License
+
+This project is distributed under the [MIT License](https://opensource.org/licenses/MIT), see [LICENSE](./LICENSE) for more information.
--- model.py
+++ model.py
@@ -4,24 +4,19 @@ import torch.nn.functional as F
 
 MAX_LENGTH = 15
 
-"""
-Encoder RNN Model
-"""
+
 class EncoderRNN(nn.Module):
-    
-    def __init__(self, input_size, hidden_size, num_layers=1):
+    """Encoder RNN Model encodes the source sequence into a dense embedding
+    that the decoder hidden weights are initialized/conditioned on.
+    """
+
+   def __init__(self, input_size, hidden_size, num_layers=1):
         super(EncoderRNN, self).__init__()
-        
-        # Model Config
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-
-        # Embedding Layer
         self.embedding = nn.Embedding(input_size, self.hidden_size)
-        
-        # LSTM Layers
-        self.rnn = nn.LSTM(self.hidden_size, self.hidden_size, 
-                            self.num_layers, bidirectional=True)
+        self.rnn = nn.LSTM(self.hidden_size, self.hidden_size,
+                           self.num_layers, bidirectional=True)
 
     def forward(self, input_seq, hidden):
         embedded = self.embedding(input_seq).view(1, 1, -1)
@@ -31,58 +26,54 @@ class EncoderRNN(nn.Module):
             output[:, :, :self.hidden_size], output[:, :, self.hidden_size:]
         output = torch.cat((forward_output, backward_output))
         return output, hidden
-    
-    def init_hidden(self):
-        return (torch.zeros(self.num_layers * 2, 1, 
-                self.hidden_size, device=torch.device('cuda')),
-                torch.zeros(self.num_layers * 2, 1, 
-                self.hidden_size, device=torch.device('cuda')))
 
-"""
-Attention Decoder RNN Model
-"""
+    def init_hidden(self):
+        return (torch.zeros(self.num_layers * 2, 1,
+                            self.hidden_size, device=torch.device('cuda')),
+                torch.zeros(self.num_layers * 2, 1,
+                            self.hidden_size, device=torch.device('cuda')))
+
+
 class AttnDecoderRNN(nn.Module):
+    """Attention Decoder RNN Model hidden weights are conditioned on encoding
+    of source sequence from encoder. It generates probability distribution on 
+    the target vocabulary after applying attention on each time-step.
+    """
     
-    def __init__(self, hidden_size, output_size, num_layers=1, 
-                dropout_p=0.1, max_length=MAX_LENGTH):
+
+    def __init__(self, hidden_size, output_size, num_layers=1,
+                 dropout_p=0.1, max_length=MAX_LENGTH):
         super(AttnDecoderRNN, self).__init__()
-        
-        # Model Config
         self.hidden_size = hidden_size
         self.output_size = output_size
         self.dropout_p = dropout_p
         self.max_length = max_length
         self.num_layers = num_layers
-        
-        # Embedding Layer
+
         self.embedding = nn.Embedding(self.output_size, self.hidden_size)
         
-        # Attention Mechanism
         self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
         self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
         
-        # Dropout
         self.dropout = nn.Dropout(self.dropout_p)
         
-        # LSTM Layers
-        self.rnn = nn.LSTM(self.hidden_size, self.hidden_size, 
-                            self.num_layers, bidirectional=True)
-        # Output Dense    
+        self.rnn = nn.LSTM(self.hidden_size, self.hidden_size,
+                           self.num_layers, bidirectional=True)
         self.out = nn.Linear(self.hidden_size, self.output_size)
-        
+
     def forward(self, input_seq, hidden, encoder_outputs):
         embedded = self.embedding(input_seq).view(1, 1, -1)
         embedded = self.dropout(embedded)
 
-        attn_weights = F.softmax( self.attn(
+        attn_weights = F.softmax(self.attn(
             torch.cat((embedded[0], hidden[0][0]), 1)), dim=1)
 
-        attn_applied = torch.bmm(attn_weights.unsqueeze(0), 
+        attn_applied = torch.bmm(attn_weights.unsqueeze(0),
                                  encoder_outputs.unsqueeze(0))
-        
+
         output = torch.cat((embedded[0], attn_applied[0]), 1)
         output = self.attn_combine(output).unsqueeze(0)
-        
+
         output = F.relu(output)
         output, hidden = self.rnn(output, hidden)
         forward_output, backward_output = \
@@ -90,9 +81,9 @@ class AttnDecoderRNN(nn.Module):
         output = torch.cat((forward_output, backward_output))
         output = F.log_softmax(self.out(output[0]), dim=1)
         return output, hidden, attn_weights
-    
+
     def init_hidden(self):
-        return (torch.zeros(self.num_layers * 2, 1, 
-                self.hidden_size, device=torch.device('cuda')),
-                torch.zeros(self.num_layers * 2, 1, 
-                self.hidden_size, device=torch.device('cuda')))
+        return (torch.zeros(self.num_layers * 2, 1,
+                            self.hidden_size, device=torch.device('cuda')),
+                torch.zeros(self.num_layers * 2, 1,
+                            self.hidden_size, device=torch.device('cuda')))
